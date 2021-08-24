71301eff099f149fb674739769a71d79e23b09de
Administrator <Administrator@host.docker.internal>
2021-08-24 16:10:54 +0530

    First Commit

---
+++ src/Calculator.java
@@ -0,0 +1,38 @@
+
+import java.util.HashMap;
+
+public class Calculator {
+	static	HashMap<Integer,String> emps=new HashMap<Integer,String>();
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	public static int div(int a,int b)
+	{
+		return a/b;
+	}
+	public static int addEmployee(int empId ,String empName)
+	{
+		emps.put(empId,empName);
+	return	emps.size();//1
+	}
+	public static String  updateEmployee(int empId ,String empName)
+	{
+		return emps.put(empId,empName);
+	//return	emps.size();//1
+	}
+	public static HashMap<Integer,String> fetchAllEmployees()
+	{
+		return emps;
+	//return	emps.size();//1
+	}
+	public static String deleteEmployee(int empId)
+	{
+		return emps.remove(111);
+		
+	}
+	public static String fetchEmp(int empId)
+	{
+		return emps.get(empId);
+	}
+}
---
+++ src/Testcases.java
@@ -0,0 +1,88 @@
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+
+import org.junit.Ignore;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+public class Testcases {
+	@BeforeAll
+	public static void m1() {
+		System.out.println("Executed BeforeALL and once");
+	}
+	@AfterAll
+	public static void m2() {
+		System.out.println("Execute AfterALL and once");
+	}
+	@AfterEach
+	public  void m3() {
+		System.out.println("Execute AfterEach testcase ");
+	}
+	@BeforeEach
+	public  void m4() {
+		System.out.println("Execute BeforeEach testcase ");
+	}
+	@Test
+	public void testAdd()
+	{
+		System.out.println("test1");
+		int addition=Calculator.add(123, 124);
+		
+		assertEquals(247,addition);
+	}
+	/*
+	@Test 
+	public void testAddEmployee()
+	{
+		int result=Calculator.addEmployee(111,"sandeep");
+		Calculator.addEmployee(112,"sangavi");
+    
+		assertEquals(2,result);
+				
+	}
+	@Test
+	public void testUpdateEmployee()
+	{
+		
+	String oldName=Calculator.updateEmployee(111,"pratheek");
+		assertEquals("sandeep",oldName);
+		
+	}
+	@Test
+	public void testDeleteEmployee()
+	{
+		
+	String deleteName=Calculator.deleteEmployee(111);
+		assertEquals("pratheek",deleteName);
+		
+	}
+	@Test
+	public void testFetchEmployee()
+	{
+		
+	String fetched=Calculator.fetchEmp(112);
+		assertEquals("sangavi",fetched);
+		
+	}
+	/*@Test
+	public void testFetchAllEmployee()
+	{
+		
+	HashMap<Integer, String> fetched=Calculator.fetchAllEmployees();
+		assertEquals("sangavi",fetched);
+		
+	}*/
+	//@Test
+	
+ public void testdiv()
+	{
+		System.out.println("test3");
+		int result=Calculator.div(12,2);
+		
+		assertEquals(6,result);
+	}
+}
